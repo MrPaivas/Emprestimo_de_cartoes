0e29769ba5c5fc6a6943909d2d2dacb6970c7bdf
PaivaIT <tiago.paiva@anhanguera.com>
2023-12-23 00:12:46 -0300

    add time zone america/sao paulo

--- untils/formated_datetime.py
+++ untils/formated_datetime.py
@@ -1,12 +1,16 @@
 from datetime import datetime
+import pytz
 
 def date_now(db_log: bool = False):
-    """retorna data e hora foramatada para cada ambiente"""
+    """retorna data e hora formatada para cada ambiente"""
+    
+    brt_timezone = pytz.timezone('America/Sao_Paulo')
+    
     if db_log:
-        data_hora = datetime.now()
+        data_hora = datetime.now(brt_timezone)
         return data_hora.strftime("%Y-%m-%dT%H:%M:%S")
     else:
-        data_hora = datetime.now()
+        data_hora = datetime.now(brt_timezone)
         return data_hora.strftime("%d/%m/%Y %H:%M:%S")
     
 def format_date(date):
