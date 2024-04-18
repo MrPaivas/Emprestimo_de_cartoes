783f96f0ae298cc9db343c9f15fec35e8aebe0ea
PaivaIT <tiago.paiva@outlook.com.br>
2024-04-17 23:57:35 -0300

    Change DB

--- config/config.py
+++ config/config.py
@@ -5,8 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 load_dotenv()
 db = SQLAlchemy()
 
+PASSWORD = os.environ.get('DB_PASS')
+PUBLIC_IP_ADDRESS = os.environ.get('PUBLIC_IP_ADDRESS')
+DBNAME = os.environ.get('DB_NAME')
+PROJECT_ID = os.environ.get('PROJECT_ID')
+INSTANCE_NAME = os.environ.get('INSTANCE_NAME')
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = True
